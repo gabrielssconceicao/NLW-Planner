5d64f6edd7a4e0d8628944d86557c06edfbaf2a5
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-07-10 18:33:54 -0300

    trip-details:separating important links

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -10,6 +10,7 @@ import {
 } from 'lucide-react';
 import { useState } from 'react';
 import { CreateActivityModal } from './create-activity-modal';
+import { ImportantLinks } from './important-links';
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
@@ -92,41 +93,7 @@ export function TripDetailsPage() {
           </div>
         </div>
         <div className='w-80 space-y-6'>
-          <div className='space-y-6'>
-            <h2 className='font-semibold text-xl'>Links importantes</h2>
-            <div className='space-y-6'>
-              <div className='flex items-center justify-between gap-4'>
-                <div className='space-y-1.5'>
-                  <span className='block font-medium text-zinc-100'>
-                    Reserva do AirBnB
-                  </span>
-                  <a
-                    href='#'
-                    className='block text-xs text-zinc-400 truncate hover:text-zinc-200'>
-                    https://www.airbnb.com.br/rooms/85048948958484643609347690437
-                  </a>
-                </div>
-                <Link2 className='size-5 text-zinc-400 shrink-0' />
-              </div>
-              <div className='flex items-center justify-between gap-4'>
-                <div className='space-y-1.5'>
-                  <span className='block font-medium text-zinc-100'>
-                    Reserva do AirBnB
-                  </span>
-                  <a
-                    href='#'
-                    className='block text-xs text-zinc-400 truncate hover:text-zinc-200'>
-                    https://www.airbnb.com.br/rooms/85048948958484643609347690437
-                  </a>
-                </div>
-                <Link2 className='size-5 text-zinc-400 shrink-0' />
-              </div>
-            </div>
-            <button className='w-full bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-zinc-700'>
-              <Plus className='size-5' />
-              Cadastrar novo link
-            </button>
-          </div>
+          <ImportantLinks />
           <div className='w-full h-px bg-zinc-800' />
 
           <div className='space-y-6'>
