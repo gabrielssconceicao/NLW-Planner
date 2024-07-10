faa5f54efddc8d94734f2d45db48723d324c20bc
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-07-10 17:59:12 -0300

    trip-details: guests

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,10 +1,12 @@
 import {
   Calendar,
   CircleCheck,
+  CircleDashed,
   Link2,
   MapPin,
   Plus,
   Settings2,
+  UserCog,
 } from 'lucide-react';
 
 export function TripDetailsPage() {
@@ -112,6 +114,40 @@ export function TripDetailsPage() {
             </button>
           </div>
           <div className='w-full h-px bg-zinc-800' />
+
+          <div className='space-y-6'>
+            <h2 className='font-semibold text-xl'>Convidados</h2>
+            <div className='space-y-6'>
+              <div className='flex items-center justify-between gap-4'>
+                <div className='space-y-1.5'>
+                  <span className='block font-medium text-zinc-100'>
+                    Jessica White
+                  </span>
+                  <span className='block text-sm text-zinc-400 truncate '>
+                    jessica.white44@yahoo.com
+                  </span>
+                </div>
+
+                <CircleDashed className='size-5 text-zinc-400 shrink-0' />
+              </div>
+              <div className='flex items-center justify-between gap-4'>
+                <div className='space-y-1.5'>
+                  <span className='block font-medium text-zinc-100'>
+                    Dr. Rita Pacocha
+                  </span>
+                  <span className='block text-sm text-zinc-400 truncate '>
+                    lasy.stiedmann@gmail.com
+                  </span>
+                </div>
+
+                <CircleDashed className='size-5 text-zinc-400 shrink-0' />
+              </div>
+            </div>
+            <button className='w-full bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-zinc-700'>
+              <UserCog className='size-5' />
+              Gerenciar Convidados
+            </button>
+          </div>
         </div>
       </main>
     </div>
