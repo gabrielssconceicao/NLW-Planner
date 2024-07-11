9baef0143122a3c2102b89a1422b47b3835c1b7e
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-07-11 11:37:15 -0300

    Create-trip: validate date

--- backend/src/routes/create-trip.ts
+++ backend/src/routes/create-trip.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import dayjs from 'dayjs';
 import { z } from 'zod';
 import { prisma } from '../lib/prisma';
 export async function createTrip(app: FastifyInstance) {
@@ -16,6 +17,15 @@ export async function createTrip(app: FastifyInstance) {
     },
     async (request) => {
       const { destination, ends_at, starts_at } = request.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error('Invalid trip start date');
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error('Invalid trip end date');
+      }
+
       const trip = await prisma.trip.create({
         data: { destination, starts_at, ends_at },
       });
