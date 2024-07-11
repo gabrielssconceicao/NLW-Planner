d8182f90b11dac809c57412721658ba5d5651810
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-07-11 11:07:59 -0300

    Starting create-trip

--- backend/src/routes/create-trip.ts
+++ backend/src/routes/create-trip.ts
@@ -1,7 +1,27 @@
 import { FastifyInstance } from 'fastify';
-
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma';
 export async function createTrip(app: FastifyInstance) {
-  app.post('/trips', async () => {
-    return 'Hello';
-  });
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/trips',
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+        }),
+      },
+    },
+    async (request) => {
+      const { destination, ends_at, starts_at } = request.body;
+      const trip = await prisma.trip.create({
+        data: { destination, starts_at, ends_at },
+      });
+      return {
+        tripId: trip.id,
+      };
+    }
+  );
 }
